8d801b27cec121c13a03f53226b54bf4ca1556e8
Saimirz <106036032+Saimirz@users.noreply.github.com>
2022-12-24 02:01:03 -0500

    Created Renters class in Entity

--- src/main/java/com/example/zoomrentals/entity/Renters.java
+++ src/main/java/com/example/zoomrentals/entity/Renters.java
@@ -1,12 +1,27 @@
 package com.example.zoomrentals.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "renters")
 public class Renters {
-    
+    @Id @GeneratedValue
+    private long id;
+    @Column(name = "first_name")
+            private String firstName ;
+    @Column (name = "last name")
+            private String lastName ;
+    @Column (name="email")
+            private String email;
+    @Column (name = "Number")
+             private String number;
+    @Column (name = "Address")
+             private  String Address;
+
+
+
 }
