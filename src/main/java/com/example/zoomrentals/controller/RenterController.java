a08e572df614223d9e70bc647ef6c007ef68b6e7
Saimirz <106036032+Saimirz@users.noreply.github.com>
2022-12-24 02:40:51 -0500

    Implemented method to get renterResponses.

--- src/main/java/com/example/zoomrentals/controller/RenterController.java
+++ src/main/java/com/example/zoomrentals/controller/RenterController.java
@@ -2,12 +2,14 @@ package com.example.zoomrentals.controller;
 
 
 import com.example.zoomrentals.entity.Renter;
+import com.example.zoomrentals.response.RenterResponse;
 import com.example.zoomrentals.service.RenterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,7 +20,18 @@ public class RenterController {
     RenterService renterService;
 
     @GetMapping()
-    public List<Renter> getAllRenters(){
-        return renterService.getAllRenters();
+    public List<RenterResponse> getAllRenters(){
+        List<Renter> renters = renterService.getAllRenters();
+
+        List<RenterResponse> renterResponses = new ArrayList<>();
+
+        renters.forEach(renter -> {renterResponses.add(new RenterResponse(renter));
+        });
+
+        return renterResponses;
+
+
+
+
     }
 }
