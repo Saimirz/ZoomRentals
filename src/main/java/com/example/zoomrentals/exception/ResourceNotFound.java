c752142ae4d4148ce0768ccb719cd3a1d0cd066d
Saimirz <106036032+Saimirz@users.noreply.github.com>
2022-12-24 03:28:16 -0500

    added the exception package and the ResourceNotFound class.

---
+++ src/main/java/com/example/zoomrentals/exception/ResourceNotFound.java
@@ -0,0 +1,19 @@
+package com.example.zoomrentals.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFound extends RuntimeException{
+
+
+    public  ResourceNotFound(){
+        super("Resource not found");
+    }
+
+
+
+    public ResourceNotFound(String message){
+        super(message);
+    }
+}
