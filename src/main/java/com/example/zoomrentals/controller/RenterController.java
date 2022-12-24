a5ab117edbaa777e31edf9fc076d101fce8e3fee
Saimirz <106036032+Saimirz@users.noreply.github.com>
2022-12-24 03:00:03 -0500

    made a post request.
    added a dependency.

--- src/main/java/com/example/zoomrentals/controller/RenterController.java
+++ src/main/java/com/example/zoomrentals/controller/RenterController.java
@@ -2,12 +2,11 @@ package com.example.zoomrentals.controller;
 
 
 import com.example.zoomrentals.entity.Renter;
+import com.example.zoomrentals.request.RenterRequest;
 import com.example.zoomrentals.response.RenterResponse;
 import com.example.zoomrentals.service.RenterService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,8 +29,11 @@ public class RenterController {
 
         return renterResponses;
 
+    }
 
+    @PostMapping()
 
+    public void addRenter(@RequestBody RenterRequest renterRequest){
 
     }
 }
