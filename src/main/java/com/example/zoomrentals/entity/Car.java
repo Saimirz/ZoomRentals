cb4e774103cbc1aff63f87bdc474c0a7baa00dbe
Saimirz <106036032+Saimirz@users.noreply.github.com>
2022-12-24 14:31:51 -0500

    Created car Entity for one to one relationship

---
+++ src/main/java/com/example/zoomrentals/entity/Car.java
@@ -0,0 +1,25 @@
+package com.example.zoomrentals.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "brand", nullable = false)
+    private String brand;
+    @Column(name = "model", nullable = false)
+    private String model;
+    @Column(name = "type", nullable = false)
+    private String type;
+}
